926c2cfd279296ae7e4e88c1e55d8f15cfb41d0f
JUSTCHILL098 <sandy2761910@gmail.com>
2025-12-13 22:38:09 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-import website_name from "@/src/config/website.js";
 import Spotlight from "@/src/components/spotlight/Spotlight.jsx";
 import Trending from "@/src/components/trending/Trending.jsx";
 import CategoryCard from "@/src/components/categorycard/CategoryCard.jsx";
@@ -10,24 +9,30 @@ import { useHomeInfo } from "@/src/context/HomeInfoContext.jsx";
 import Schedule from "@/src/components/schedule/Schedule";
 import ContinueWatching from "@/src/components/continue/ContinueWatching";
 import TabbedAnimeSection from "@/src/components/tabbed-anime/TabbedAnimeSection";
-import JoinRoomPanel from "../../components/multiplayer/JoinRoomPanel.jsx";
+import JoinRoomPanel from "@/src/components/multiplayer/JoinRoomPanel.jsx";
 
 function Home() {
   const { homeInfo, homeInfoLoading, error } = useHomeInfo();
+
   if (homeInfoLoading) return <Loader type="home" />;
-  if (error) return <Error />;
-  if (!homeInfo) return <Error error="404" />;
+  if (error || !homeInfo) return <Error />;
+
+  console.log("✅ TOPTEN DATA =>", homeInfo.topten);
+
   return (
     <>
       <JoinRoomPanel />
+
       <div className="pt-16 w-full">
         <Spotlight spotlights={homeInfo.spotlights} />
+
         <div className="mt-6">
           <Genre data={homeInfo.genres} />
         </div>
+
         <ContinueWatching />
-        
-        <div className="w-full grid grid-cols-[minmax(0,75%),minmax(0,25%)] gap-x-6 max-[1200px]:flex flex-col">
+
+        <div className="w-full grid grid-cols-[minmax(0,75%),minmax(0,25%)] gap-x-6 max-[1200px]:flex max-[1200px]:flex-col">
           <div>
             <CategoryCard
               label="Latest Episode"
@@ -36,16 +41,21 @@ function Home() {
               path="recently-updated"
               limit={12}
             />
+
             <Schedule className="mt-8" />
-            <TabbedAnimeSection 
+
+            <TabbedAnimeSection
               topAiring={homeInfo.top_airing}
               mostFavorite={homeInfo.most_favorite}
               latestCompleted={homeInfo.latest_completed}
               className="mt-8"
             />
           </div>
+
           <div className="w-full mt-[60px]">
             <Trending trending={homeInfo.trending} />
+
+            {/* ✅ NOW THIS WORKS */}
             <Topten data={homeInfo.topten} className="mt-12" />
           </div>
         </div>
