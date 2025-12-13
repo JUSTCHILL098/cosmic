51c50d613e439f09c225a2e41df0b17f9b7cae6d
JUSTCHILL098 <sandy2761910@gmail.com>
2025-12-14 00:01:44 +0530

    Update Link.jsx

--- src/components/navbar/Link.jsx
+++ src/components/navbar/Link.jsx
@@ -1,8 +1,6 @@
+import React from "react";
 
-import React from 'react';
-
-// Simplified Link component for SPA behavior mock
-const Link = ({ href, children, ...props }: any) => {
+const Link = ({ href, children, ...props }) => {
   return (
     <a href={href} {...props}>
       {children}
